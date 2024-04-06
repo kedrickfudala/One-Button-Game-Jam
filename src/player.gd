622c311dde28760d10f5fc3fdc851764dc74fb2a
kedrickfudala <KedrickAFudala@gmail.com>
2024-04-05 22:44:59 -0400

    added chamber visual

--- src/player.gd
+++ src/player.gd
@@ -7,15 +7,18 @@ class_name Player
 @onready var animation_player : Object = $AnimationPlayer
 @onready var enemy_spawner1 : Object = $EnemySpawner1
 @onready var enemy_spawner2 : Object = $EnemySpawner2
+@onready var chamber_sprite : Object = $ChamberSprite
 
-@onready var chamber = ["RED", "ORA", "YEL", "GRE", "BLU", "PUR"]
+@onready var chamber = ["RED", "ORANGE", "YELLOW", "GREEN", "BLUE", "PURPLE"]
 @onready var chamber_slot : float = 0.0
+@onready var chamber_spin : float = 0.0
+
 @onready var hearts : int = 3
 @onready var facing_direction : int = 1
 @onready var speed : int = 15
 
 func _ready():
-	input_timer.wait_time = 0.3
+	input_timer.wait_time = 0.15
 	input_timer.one_shot = true
 
 func _physics_process(delta):
@@ -24,22 +27,32 @@ func _physics_process(delta):
 	update_animations()
 
 func take_input():
-	var previous_slot = chamber_slot
+	if get_global_mouse_position().x > self.global_position.x:
+		facing_direction = 1
+	else:
+		facing_direction = -1
+	
 	if Input.is_action_just_pressed("ui_accept"):
 		input_timer.start()
 	if Input.is_action_pressed("ui_accept"):
-		chamber_slot += 0.1
-		if chamber_slot > 6:
-			chamber_slot -= 6
+		chamber_spin += 0.05
+		if chamber_spin > 6:
+			chamber_spin -= 6
 	if Input.is_action_just_released("ui_accept"):
 		if !input_timer.is_stopped():
 			print("Tap!")
-			chamber_slot = previous_slot
-			facing_direction *= -1
-		else:
-			print("Hold!")
-			chamber_slot = floor(chamber_slot)
 			print("FIRE " + str(chamber[chamber_slot]))
+			chamber_spin = 0
+		else:
+			chamber_slot += snapped(chamber_spin, 1) #floor(chamber_spin)
+			print(chamber_slot)
+			if chamber_slot > 5:
+				chamber_slot -= 5
+			print("Hold!")
+			print("FIRE " + str(chamber[chamber_slot]))
+			chamber_spin = 0
+	
+	chamber_sprite.rotation_degrees = (chamber_slot * -60) + (chamber_spin * -60)
 
 func move():
 	velocity = Vector2(speed * facing_direction, 0)
